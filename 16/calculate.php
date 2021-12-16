16159591ffdb175bd59f5f5ee9db06a58ef07d8e
Miguel Barahona <migbara@gmail.comm>
2021-12-16 17:35:32 +0100

    Test Day 16

---
+++ 16/calculate.php
@@ -0,0 +1,274 @@
+<?php
+
+//AOC - Day 16
+//php.exe -c "c:\php8.1.0\php.ini" "C:\....\aoc_2021\16\calculate.php"
+
+function getData(){
+    //1 - Read from a file
+    $file = dirname($_SERVER["SCRIPT_FILENAME"])."\\input2.txt";
+    $f = fopen($file,"r");
+
+    $data = array();
+    while(!feof($f)){
+        $line = rtrim(fgets($f));
+        $row = str_split(rtrim($line));
+        //array_push($data,$row);
+        $data = $row;
+    }
+    return $data;
+}
+
+function convertBinary($data){
+    $conv = array("0"=>"0000","1"=>"0001","2"=>"0010","3"=>"0011","4"=>"0100","5"=>"0101","6"=>"0110","7"=>"0111","8"=>"1000","9"=>"1001","A"=>"1010","B"=>"1011","C"=>"1100","D"=>"1101","E"=>"1110","F"=>"1111");
+    $res = '';
+    foreach($data as $k => $h){
+        echo $h."\r\n";
+        $res .= $conv[$h];
+    }
+    return $res;
+}
+
+function readData(&$cad){
+    $packets = array();
+    while(strlen($cad)>0 && trim($cad,"0")!=''){
+
+        $pos = 0;
+        
+        $v = substr($cad,$pos,3);
+        $pos += 3;
+        $t = substr($cad,$pos,3);
+        $pos += 3;
+
+        $vdec = bindec($v);
+
+        $packet = array("cad"=>$cad,"v"=>$v,"vdec"=>$vdec,"t"=>$t);
+
+        $b = '';
+        $dec = '';
+        $length = '';
+        $operator = '';
+        $indicator = '';
+
+        $numbers = array();
+
+        if($t == "100"){
+            //literal
+
+            $length = 0;
+            $start = 6;
+            $size = 5;
+            $p = substr($cad,$start,$size);
+            $pos += $size;
+            //add numbers starting with 1
+            while(substr($p,0,1)!='0'){
+                $numbers[] = substr($p,1);
+                $start += $size;
+                $p = substr($cad,$start,$size);
+                $pos+=$size;
+            }
+            //add packet starting with 0
+            $numbers[] = substr($p,1);
+
+            $b = implode("",$numbers);
+
+            $dec = bindec($b);
+
+            $packet["numbers"] = $numbers;
+            $packet["bin"] = $b;
+            $packet["dec"] = $dec;
+
+            $packets[] = $packet;
+
+            $cad = substr($cad,$pos);
+        }
+        else{
+            //operator
+            $indicator = substr($cad,$pos,1);
+            $pos+=1;
+            if($indicator=='0'){
+                $nbits = 15;
+
+                $length_bin = substr($cad,$pos,$nbits);
+                $pos+=$nbits;
+                $length = bindec($length_bin);
+            }
+            else{
+                $nbits = 11;
+
+                $num_subpackets_bin = substr($cad,$pos,$nbits);
+                $pos+=$nbits;
+
+                $length = bindec($num_subpackets_bin) * $nbits;
+            }
+
+            
+            $ncad = substr($cad,$pos);
+            //$pos+=$length;
+
+            
+
+            echo "NUEVA CADENA ".$ncad."\r\n";
+            $operator = readData($ncad);
+
+            $packet["length"] = $length;
+            $packet["indicator"] = $indicator;
+            $packet["operator"] = $operator;
+
+            $packets[] = $packet;
+
+            $cad = $ncad;
+
+        }
+        
+
+    }
+
+    
+    return $packets;
+}
+
+function addVersions($data){
+    $sum = 0;
+    foreach($data as $v){
+        $sum += $v["vdec"];
+        if(isset($v["operator"]))
+            $sum += addVersions($v["operator"]);
+    }
+    return $sum;
+}
+
+function calculate($data){
+    $res0 = 0;
+    $res1 = 1;
+    $res2 = PHP_INT_MAX;
+    $res3 = 0;
+    $res5 = 0;
+    $res6 = 0;
+    $res7 = 0;
+
+    $cont = 0;
+    foreach($data as $v){
+        if(isset($v["dec"])){
+
+            echo "VDEC ".$v["dec"]."\r\n";
+
+
+            $res0 += $v["dec"];
+            $res1 *= $v["dec"];
+
+            
+            
+            if($v["dec"]<$res2)
+                $res2 = $v["dec"];
+            echo "010 " . $res2."\r\n";
+
+            if($v["dec"]>$res3)
+                $res3 = $v["dec"];
+            echo "011 " . $res3."\r\n";
+
+            if($cont==0){
+                $res5_ant = $v["dec"];
+                $res6_ant = $v["dec"];
+                $res7_ant = $v["dec"];
+            }
+            if($cont>0 && $v["dec"]>$res5_ant)
+                $res5 = 1;
+            echo "101 " . $res5."\r\n";
+            if($cont>0 && $v["dec"]<$res6_ant)
+                $res6 = 1;
+            echo "110 " . $res6."\r\n";
+            if($cont>0 && $v["dec"]==$res7_ant)
+                $res7 = 1;
+            echo "111 " . $res7."\r\n";
+        }
+        if($v["t"]=='000'){
+            //sum packets
+            if(isset($v["operator"])){
+                $c= calculate($v["operator"]);
+                $res0 += $c["res0"];
+            }
+        }
+        if($v["t"]=='001'){
+            //product packets
+            if(isset($v["operator"])){
+                $c= calculate($v["operator"]);
+                $res1 *= $c["res1"];
+            }
+        }
+        if($v["t"]=='010'){
+            //minimum packets
+            if(isset($v["operator"])){
+                $c= calculate($v["operator"]);
+                // echo "010 " . $c["res2"]."\r\n";
+                if($c["res2"]<$res2)
+                    $res2 = $c["res2"];
+            }
+        }
+        if($v["t"]=='011'){
+            //minimum packets
+            if(isset($v["operator"])){
+                $c= calculate($v["operator"]);
+                echo "011 " . $c["res3"]."\r\n";
+                if($c["res3"]>$res3)
+                    $res3 = $c["res3"];
+            }
+        }
+        if($v["t"]=='101'){
+            //greater than packets
+            if(isset($v["operator"])){
+                $c= calculate($v["operator"]);
+                echo "101 " . $c["res5"]."\r\n";
+                $res5 = $c["res5"];
+            }
+        }
+        if($v["t"]=='110'){
+            //greater than packets
+            if(isset($v["operator"])){
+                $c= calculate($v["operator"]);
+                echo "110 " . $c["res6"]."\r\n";
+                $res6 = $c["res6"];
+            }
+        }
+        if($v["t"]=='111'){
+            //greater than packets
+            if(isset($v["operator"])){
+                $c= calculate($v["operator"]);
+                echo "111 " . $c["res7"]."\r\n";
+                $res7 = $c["res7"];
+            }
+        }
+        $cont++;
+    }
+    return ["res0"=>$res0,"res1"=>$res1,"res2"=>$res2,"res3"=>$res3,"res5"=>$res5,"res6"=>$res6,"res7"=>$res7];
+}
+
+$data = getData();
+
+print_r($data);
+
+$binary = convertBinary($data);
+
+echo $binary."\r\n";
+
+$data = readData($binary);
+
+print_r($data);
+
+$sum = addVersions($data);
+
+echo "SUMA ".$sum."\r\n";
+
+$res = calculate($data);
+
+echo "RES0 ".$res["res0"]." RES1 ".$res["res1"]." RES2 ".$res["res2"]." RES3 ".$res["res3"]." RES5 ".$res["res5"]." RES6 ".$res["res6"]." RES7 ".$res["res7"]."\r\n";
+
+// calculate_part1($data);
+// calculate_part2($data);
+
+
+
+
+
+
+
+
